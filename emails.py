8ee757721c8fa490049e80a506b37955518256c7
Howstheaqua <38701438+Howstheaqua@users.noreply.github.com>
2020-02-03 14:31:23 -0500

    Add files via upload

---
+++ emails.py
@@ -0,0 +1,6 @@
+#Gmail						 smtp.gmail.com
+#Outlook.com/Hotmail.com	 smtp-mail.outlook.com
+#Yahoo Mail					 smtp.mail.yahoo.com
+#AT&T						 smpt.mail.att.net (port 465)
+#Comcast 				   	 smtp.comcast.net
+#Verizon 					 smtp.verizon.net (port 465)
